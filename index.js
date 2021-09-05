908a11ca946c522fdfc984b2a69674497a8e95c5
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2021-09-06 03:38:48 +0530

    Updated Local Storage Integration
    Updated Local Storage Integration for Edit and Delete

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ var formData = document.getElementById("form-data");
 //to add following elements;
 /* 
 <div class="todo-item">
-<p class="todo-text">Buy Mangoes</p>
-<input type="text" value="Buy Mangoes">
-<div class="icon-wrapper">
-    <i class="far fa-check-square" id="tick"></i>
-    <i class="fas fa-pen" id="edit"></i>
-    <i class="fas fa-trash-alt" id="del"></i>
-    <i class="fas fa-save"></i>
-</div>
+    <p class="todo-text">Buy Mangoes</p>
+    <input type="text" value="Buy Mangoes">
+    <div class="icon-wrapper">
+        <i class="far fa-check-square" id="tick"></i>
+        <i class="fas fa-pen" id="edit"></i>
+        <i class="fas fa-trash-alt" id="del"></i>
+        <i class="fas fa-save"></i>
+    </div>
 </div> 
 */
 
-function addItems(text){
+function addItems(text, ID){
     var addDiv = document.createElement("div");
     addDiv.classList.add("todo-item");
+    addDiv.id = ID;
     mainList.appendChild(addDiv); 
 
     var addPara = document.createElement("p");
@@ -29,7 +30,8 @@ function addItems(text){
     var inputbox = document.createElement("input");
     inputbox.type = "text";
     inputbox.value = text;
-    inputbox.id = "input-text2";
+    inputbox.className = "input-text2";
+    inputbox.id = ID;
     addDiv.appendChild(inputbox);
     inputbox.style.display="none";
 
@@ -57,7 +59,27 @@ function addItems(text){
     addIcon4.id= "save";
     anotherDiv.appendChild(addIcon4);
     addIcon4.style.display="none";
+
+    function updateTodo(newtext, newID){
+        console.log("newtext", newtext);
+        console.log("newID", newID);
+        var tempStorage = localStorage.getItem("Todo");
+        if(tempStorage !== null && tempStorage !== undefined){
+            tempStorage = JSON.parse(localStorage.getItem("Todo"));
+            var positionFinder= -1;
+            for(var i=0; i<tempStorage.length; i++){
+                if(tempStorage[i].id == newID){
+                    positionFinder = i;
+                    break;
+                }
+            }
+            tempStorage[positionFinder].value = newtext;
+            localStorage.setItem("Todo",JSON.stringify(tempStorage));
+        }
+    }
+
     addIcon4.onclick = function(){
+        updateTodo(inputbox.value, inputbox.id);
         addPara.style.display="block";
         addPara.innerText = inputbox.value;
         inputbox.style.display = "none";
@@ -68,7 +90,24 @@ function addItems(text){
         addPara.style.textDecoration = "none";
     }
     
+    function deleteTodo(iD){
+        var tempStorage = localStorage.getItem("Todo");
+        if(tempStorage !==null){
+            tempStorage = JSON.parse(localStorage.getItem("Todo"));
+            var positionFinder= -1;
+            for(var i=0; i<tempStorage.length; i++){
+                if(tempStorage[i].id == iD){
+                    positionFinder = i;
+                    break;
+                }
+            }
+            tempStorage.splice(positionFinder, 1);
+            localStorage.setItem("Todo",JSON.stringify(tempStorage));
+        }
+    }
+
     addIcon3.addEventListener("click", function(){
+        deleteTodo(addDiv.id);
         addDiv.remove();
     })
     addIcon1.onclick = function(){
@@ -84,8 +123,35 @@ function addItems(text){
     }   
 }
 
-formData.addEventListener("submit", function(obj){
-    obj.preventDefault();
-    addItems(inputText.value);
+function addTodoIntoLocal(message, ID){
+    var tempStorage = localStorage.getItem("Todo");
+    if(tempStorage === null){
+        var mArr = [];
+        mArr.push({id:ID, value: message});
+        localStorage.setItem("Todo", JSON.stringify(mArr));
+    }
+    else{
+        tempStorage = JSON.parse(localStorage.getItem("Todo"));
+        tempStorage.push({id:ID, value: message});
+        localStorage.setItem("Todo", JSON.stringify(tempStorage));
+    }
+}
+
+function renderTodoCard(){
+    var tempStorage = localStorage.getItem("Todo");
+    if(tempStorage !==null && tempStorage !== undefined){
+        tempStorage = JSON.parse(localStorage.getItem("Todo"));
+        for(var i=0; i<tempStorage.length; i++){
+            addItems(tempStorage[i].value, tempStorage[i].id);
+        }
+    }
+}
+renderTodoCard();
+
+formData.addEventListener("submit", function(e){
+    e.preventDefault();
+    var todoID = new Date().getTime();
+    addItems(inputText.value, todoID);
+    addTodoIntoLocal(inputText.value, todoID);
     inputText.value = "";
 });
